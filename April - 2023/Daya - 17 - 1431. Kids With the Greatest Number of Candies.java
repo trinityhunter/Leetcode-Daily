90bbd51e95bf5709df2ef6dde6e3c40638dc0cae
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-04-17 08:45:30 +0530

    Create Daya - 17 - 1431. Kids With the Greatest Number of Candies.java

---
+++ April - 2023/Daya - 17 - 1431. Kids With the Greatest Number of Candies.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies){
+
+        List<Boolean> arr = new ArrayList<>();
+        
+        int max = Integer.MIN_VALUE;
+        
+        for(int i=0; i<candies.length; i++){
+            max = Math.max(max, candies[i]);
+        }
+        
+        for(int i=0; i<candies.length; i++){
+            if(candies[i]+extraCandies>=max){
+                arr.add(true);
+            }
+            else{
+                arr.add(false);
+            }
+        }
+        
+        return arr;
+
+    }
+
+}
