7027a290cff733a6f55e4c3c1564d6241c3945f3
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-04-22 07:30:18 +0530

    Create Day - 22 - 1312. Minimum Insertion Steps to Make a String Palindrome.java

---
+++ April - 2023/Day - 22 - 1312. Minimum Insertion Steps to Make a String Palindrome.java
@@ -0,0 +1,80 @@
+class Solution {
+
+    public static int helper(String x, String y, int ind1, int ind2){
+
+        if(ind1<0 || ind2<0){
+            return 0;
+        }
+
+        if(dp[ind1][ind2]!=null){
+            return dp[ind1][ind2];
+        }
+
+        if(x.charAt(ind1) == y.charAt(ind2)){
+            return dp[ind1][ind2] = 1 + helper(x, y, ind1-1, ind2-1);
+        }
+
+        return dp[ind1][ind2] = Math.max(helper(x, y, ind1-1, ind2), helper(x, y, ind1, ind2-1));
+
+    }
+
+    public static Integer[][] dp;
+
+    public int minInsertions(String s) {
+      
+        // Memoization - 
+        
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=s.length()-1; i>=0; i--){
+            sb.append(s.charAt(i));
+        }
+
+        if(s.equals(sb.toString())){
+            return 0;
+        }
+
+        dp = new Integer[s.length()][s.length()];
+
+        int ans = helper(s, sb.toString(), s.length()-1, sb.toString().length()-1);
+
+        return s.length() - ans;
+      
+        // Tabulation - 
+
+        // StringBuilder sb = new StringBuilder();
+
+        // for(int i=s.length()-1; i>=0; i--){
+        //     sb.append(s.charAt(i));
+        // }
+
+        // if(s.equals(sb.toString())){
+        //     return 0;
+        // }
+
+        // int[][] dp = new int[s.length()+1][s.length()+1];
+
+        // for(int i=0; i<dp.length; i++){
+        //     for(int j=0; j<dp[0].length; j++){
+        //         if(i == 0){
+        //             dp[i][j] = 0;
+        //         }
+        //         else if(j == 0){
+        //             dp[i][j] = 0;
+        //         }
+        //         else{
+        //             if(s.charAt(i-1) == sb.toString().charAt(j-1)){
+        //                 dp[i][j] = 1 + dp[i-1][j-1];
+        //             }
+        //             else{
+        //                 dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // return s.length() - dp[dp.length-1][dp[0].length-1]; 
+
+    }
+    
+}
