5c4deed0cfb53e3a3ba8e1e58b47d1676c3f3fcf
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-04-24 17:44:50 +0530

    Create Day - 24 - 1046. Last Stone Weight.java

---
+++ April - 2023/Day - 24 - 1046. Last Stone Weight.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int lastStoneWeight(int[] stones) {
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int i=0; i<stones.length; i++){
+            pq.add(stones[i]);
+        }
+
+        while(pq.size()>1){
+
+            pq.add(pq.remove() - pq.remove());
+
+        }
+
+        return pq.peek();
+
+    }
+
+}
