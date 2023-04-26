ea995fa1f49b6eecaaaabb3887ac43acc15a3abf
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-04-26 21:45:23 +0530

    Create Day - 26 - 258. Add Digits.java

---
+++ April - 2023/Day - 26 - 258. Add Digits.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+    public int addDigits(int num) {
+        
+        String str = num + "";
+        
+        if(str.length() == 1){
+            return num;
+        }
+        
+        while(str.length()!=1){
+            
+            int temp = 0;
+            
+            for(int i=0; i<str.length(); i++){
+                temp += str.charAt(i) - '0';
+            }
+            
+            str = temp + "";
+            
+        }
+        
+        return Integer.parseInt(str);
+        
+    }
+
+}
