ea971fb324c7bd5a3047f97cbc9bd0035ce018ef
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-05-08 06:04:46 +0530

    Create Day - 8 - 1572. Matrix Diagonal Sum.java

---
+++ May - 2023/Day - 8 - 1572. Matrix Diagonal Sum.java
@@ -0,0 +1,23 @@
+class Solution {
+
+    public int diagonalSum(int[][] mat) {
+        
+        int sum = 0;
+
+        for(int i=0; i<mat.length; i++){
+            sum += mat[i][i];
+        }
+
+        for(int i=mat.length-1; i>=0; i--){
+            sum += mat[i][mat.length-i-1];
+        }
+
+        if(mat.length%2 == 1){
+            sum -= mat[mat.length/2][mat.length/2];
+        }
+
+        return sum;
+
+    }
+
+}
