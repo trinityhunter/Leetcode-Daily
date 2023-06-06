7982192854497ddaae931189744cfb28e435bdc6
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-06-06 19:27:05 +0530

    Create Day - 6 - 1502. Can Make Arithmetic Progression From Sequence.java

---
+++ June - 2023/Day - 6 - 1502. Can Make Arithmetic Progression From Sequence.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public boolean canMakeArithmeticProgression(int[] arr) {
+        
+        Arrays.sort(arr);
+        
+        boolean ans = true;
+        
+        int diff = arr[1] - arr[0];
+        
+        for(int i=2; i<arr.length; i++){
+            if(diff!=arr[i] - arr[i-1]){
+                return false;
+            }
+        }
+        
+        return ans;
+    
+    }
+
+}
