5f0a94cf4460f2d9a30379a7d45c5309f5b7d73c
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-11-29 11:56:11 +0530

    Create Day - 29 - 191. Number of 1 Bits.java

---
+++ November - 2023/Day - 29 - 191. Number of 1 Bits.java
@@ -0,0 +1,17 @@
+public class Solution {
+    
+    public int hammingWeight(int n) {
+
+        int count = 0;
+
+        for(int i=0; i<32; i++){
+            if((n & (1L << i)) > 0){
+                count ++;
+            }
+        }
+
+        return count;
+
+    }
+
+}
