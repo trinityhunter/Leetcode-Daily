04198b64361420c663e0d28c690939d7694e7a20
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2023-12-14 07:23:05 +0530

    Create Day - 14 - 2482. Difference Between Ones and Zeros in Row and Column.java

---
+++ December - 2023/Day - 14 - 2482. Difference Between Ones and Zeros in Row and Column.java
@@ -0,0 +1,26 @@
+class Solution {
+    
+    public int[][] onesMinusZeros(int[][] grid) {
+
+        int[] row = new int[grid.length];
+
+        int[] col = new int[grid[0].length];
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                row[i] += grid[i][j];
+                col[j] += grid[i][j];
+            }
+        }
+
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                grid[i][j] = 2*(row[i]+col[j]) - grid.length - grid[0].length;
+            }
+        }
+
+        return grid;
+
+    }
+
+}
