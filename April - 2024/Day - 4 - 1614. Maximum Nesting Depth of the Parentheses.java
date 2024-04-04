08ca5e8f938f5f11cd2d4d004e8873e2632a2608
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2024-04-04 21:52:45 +0530

    Create Day - 4 - 1614. Maximum Nesting Depth of the Parentheses.java

---
+++ April - 2024/Day - 4 - 1614. Maximum Nesting Depth of the Parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int maxDepth(String s) {
+
+        int maxDepth = 0;
+
+        int currentDepth = 0;
+
+        for(char c: s.toCharArray()){
+
+            if(c == '('){
+                currentDepth++;
+                maxDepth = Math.max(maxDepth, currentDepth);
+            } 
+            else if(c == ')'){
+                currentDepth--;
+            }
+            
+        }
+
+        return maxDepth;
+
+    }
+
+}
