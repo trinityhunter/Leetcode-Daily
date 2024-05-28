88ccda430c55a045c84422d02189909c22802cd6
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2024-05-28 07:41:10 +0530

    Create Day - 28 - 1208. Get Equal Substrings Within Budget.java

---
+++ May - 2024/Day - 28 - 1208. Get Equal Substrings Within Budget.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public int equalSubstring(String s, String t, int maxCost) {
+
+        int l = 0;
+        
+        int cost = 0;
+        
+        int result = 0;
+
+        for(int r=0; r<s.length(); r++){
+
+            cost += Math.abs(s.charAt(r) - t.charAt(r));
+
+            while(cost > maxCost){
+                cost -= Math.abs(s.charAt(l) - t.charAt(l));
+                l++;
+            }
+
+            result = Math.max(result, r-l+1);
+
+        }
+
+        return result;
+
+    }
+    
+}
