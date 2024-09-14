f59dab38c483bd5e4c90223db9d8e56f49c383be
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2024-09-14 22:51:18 +0530

    Create Day - 14 - 2419. Longest Subarray With Maximum Bitwise AND.java

---
+++ September - 2024/Day - 14 - 2419. Longest Subarray With Maximum Bitwise AND.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public int longestSubarray(int[] nums) {
+
+        int max = 0;
+        
+        int count = 0;
+        
+        int ans = 0;
+
+        for(int i: nums){
+
+            if(i == max){
+                count++;
+            }
+            else if(i > max){
+                count = ans = 1;
+                max =i ;
+            }
+            else{
+                count = 0;
+            }
+
+            ans = Math.max(count, ans);
+
+        }
+
+        return ans;
+
+    }
+    
+}
