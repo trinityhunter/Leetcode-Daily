94d9d042738a1fa47e9c7c40180f4cc404a6ae13
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2024-10-26 20:11:54 +0530

    Create Day - 26 - 2458. Height of Binary Tree After Subtree Removal Queries.java

---
+++ October - 2024/Day - 26 - 2458. Height of Binary Tree After Subtree Removal Queries.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    private Map<Integer, Integer> valToMaxHeight = new HashMap<>();
+
+    private Map<Integer, Integer> valToHeight = new HashMap<>();
+
+    private int height(TreeNode root){
+
+        if(root == null){
+            return 0;
+        }
+
+        if(valToHeight.containsKey(root.val)){
+            return valToHeight.get(root.val);
+        }
+
+        final int h = 1 + Math.max(height(root.left), height(root.right));
+
+        valToHeight.put(root.val, h);
+
+        return h;
+
+    }
+
+    private void dfs(TreeNode root, int depth, int maxHeight){
+
+        if(root == null){
+            return;
+        }
+
+        valToMaxHeight.put(root.val, maxHeight);
+
+        dfs(root.left, depth + 1, Math.max(maxHeight, depth + height(root.right)));
+
+        dfs(root.right, depth + 1, Math.max(maxHeight, depth + height(root.left)));
+
+    }
+
+    public int[] treeQueries(TreeNode root, int[] queries) {
+
+        int[] ans = new int[queries.length];
+
+        dfs(root, 0, 0);
+
+        for (int i=0; i<queries.length; ++i){
+            ans[i] = valToMaxHeight.get(queries[i]);
+        }
+
+        return ans;
+
+    }
+
+}
