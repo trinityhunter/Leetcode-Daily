c0d3f2766053ac0278cb5e4e7fc28f2b7b7a4e8a
Raviraj Bhosale <81758776+trinityhunter@users.noreply.github.com>
2024-12-16 22:24:45 +0530

    Create Day - 16 - 3264. Final Array State After K Multiplication Operations I.java

---
+++ December - 2024/Day - 16 - 3264. Final Array State After K Multiplication Operations I.java
@@ -0,0 +1,26 @@
+//class Solution {
+    //public int[] getFinalState(int[] nums, int k, int multiplier) {
+        
+    //}
+//}
+
+class Solution {
+        public int[] getFinalState(int[] nums, int k, int multiplier) {
+                while (k-- > 0) {
+                            int num = nums[0];
+                                        int index = 0;
+
+                                                   
+                                                               for (int i = 1; i < nums.length; i++) {
+                                                                               if (nums[i] < num) {
+                                                                                                   num = nums[i];
+                                                                                                                       index = i;
+                                                                                                                                       }
+                                                                                                                                                   }
+
+                                                                                                                                                               
+                                                                                                                                                                           nums[index] = nums[index] * multiplier;
+                                                                                                                                                                                   }
+                                                                                                                                                                                           return nums;
+                                                                                                                                                                                               }
+                                                                                                                                                                                               }
